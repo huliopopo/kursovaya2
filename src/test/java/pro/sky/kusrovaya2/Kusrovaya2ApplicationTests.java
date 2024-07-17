cf6eaa5ebc98bde933f7f067deda1c8bdc3a9cc5
huliopopo <yashin.hlpp@gmail.com>
2024-07-17 19:17:29 +0300

    initial commit

---
+++ src/test/java/pro/sky/kusrovaya2/Kusrovaya2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.kusrovaya2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kusrovaya2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
